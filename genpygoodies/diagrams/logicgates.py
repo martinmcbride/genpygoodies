9d4d0c0815a88e6354f35985dcfd599660290bca
Martin <mcbride.martin@gmail.com>
2023-09-09 22:30:15 +0100

    And gate, not yet drawn properly

--- genpygoodies/diagrams/logicgates.py
+++ genpygoodies/diagrams/logicgates.py
@@ -2,6 +2,7 @@
 # Created: 2023-07-29
 # Copyright (C) 2023, Martin McBride
 # License: MIT
+import math
 
 from generativepy.geometry import Polygon, Circle
 from generativepy.math import Vector as V
@@ -45,28 +46,44 @@ class Buffer(Symbol):
 
 class And(Symbol):
 
-    def __init__(self, position, width, height=None):
+    def __init__(self, position, width, height=None, invert=False):
         super().__init__(position, width, height)
-        self._connectors = (((0, self.height/4), (0, self.height*3/4)), ((self.width + self.bubble_radius*2, self.height/2),))
+        self.bubble_radius = self.width/10
+        self.invert = invert
+        output_offset = self.bubble_radius*2 if invert else 0
+        self._connectors = (((0, self.height/4), (0, self.height*3/4)), ((self.width + output_offset, self.height/2),))
 
     def draw(self, ctx):
-        a = V(self.position)
-        b = a + V(0, self.height)
-        c = a + V(self.width, self.height/2)
+        a = V(self.position) +  V(self.height/2, 0)
+        b = V(self.position)
+        c = V(self.position) + V(0, self.height)
+        d = V(self.position) + V(self.height/2, self.height)
+        centre = V(self.position) + V(self.height/2, self.height/2)
         bubble_centre = a + V(self.width + self.bubble_radius, self.height/2)
         (Polygon(ctx)
-         .of_points((a, b, c))
+         .of_points((a, b, c, d))
+         .open()
          .fill(self.fillparams.pattern, self.fillparams.fill_rule)
          .stroke(self.strokeparams.pattern, self.strokeparams.line_width, self.strokeparams.dash, self.strokeparams.cap, self.strokeparams.join,
                  self.strokeparams.miter_limit)
          )
         (Circle(ctx)
-         .of_center_radius(bubble_centre, self.bubble_radius)
+         .of_center_radius(centre, self.height/2)
+         .as_arc(math.radians(-90), math.radians(90))
          .fill(self.fillparams.pattern, self.fillparams.fill_rule)
          .stroke(self.strokeparams.pattern, self.strokeparams.line_width, self.strokeparams.dash, self.strokeparams.cap, self.strokeparams.join,
                  self.strokeparams.miter_limit)
          )
+        if self.invert:
+            (Circle(ctx)
+             .of_center_radius(bubble_centre, self.bubble_radius)
+             .fill(self.fillparams.pattern, self.fillparams.fill_rule)
+             .stroke(self.strokeparams.pattern, self.strokeparams.line_width, self.strokeparams.dash, self.strokeparams.cap, self.strokeparams.join,
+                     self.strokeparams.miter_limit)
+             )
 
+    def label_pos(self):
+        return self.position + V(self.width/3, self.height/2)
 
     def get_default_height(self):
         return self.width
--- imagetests/test_diagrams_logicgates_module.py
+++ imagetests/test_diagrams_logicgates_module.py
@@ -2,11 +2,11 @@ import math
 import unittest
 
 from generativepy.color import Color
-from generativepy.drawing import setup, make_image, CENTER, MIDDLE, RIGHT, BOTTOM, LEFT
+from generativepy.drawing import setup, make_image, CENTER, MIDDLE, RIGHT, BOTTOM, LEFT, TOP
 from generativepy.geometry import Line, Text, Circle
 from generativepy.math import Vector as V
 
-from genpygoodies.diagrams.logicgates import Buffer
+from genpygoodies.diagrams.logicgates import Buffer, And
 from image_test_helper import run_image_test
 
 """
@@ -19,7 +19,7 @@ class TestLogicGatesImages(unittest.TestCase):
         def draw(ctx, width, height, frame_no, frame_count):
             setup(ctx, width, height, background=Color(1))
 
-            gate = Buffer((200, 200), 50).fillstyle(Color("yellow")).strokestyle(Color("blue"), 3)
+            gate = Buffer((100, 100), 50).fillstyle(Color("yellow")).strokestyle(Color("blue"), 3)
             gate.draw(ctx)
             in_pos = V(gate.get_connector(0, 0))
             out_pos = V(gate.get_connector(1, 0))
@@ -30,18 +30,55 @@ class TestLogicGatesImages(unittest.TestCase):
             Text(ctx).of("B", out_pos).size(20).align(LEFT, BOTTOM).offset(10, -5).fill(Color(0))
             Text(ctx).of("X", gate.label_pos()).size(20).align(CENTER, MIDDLE).fill(Color(0))
 
-            gate = Buffer((400, 200), 50, invert=True).fillstyle(Color("yellow")).strokestyle(Color("blue"), 3)
+            gate = Buffer((100, 200), 50, invert=True).fillstyle(Color("yellow")).strokestyle(Color("blue"), 3)
             gate.draw(ctx)
-            in_pos = V(gate.get_connector(0, 0))
+            in_pos1 = V(gate.get_connector(0, 0))
             out_pos = V(gate.get_connector(1, 0))
-            Line(ctx).of_start_end(in_pos, in_pos + V(-30, 0)).stroke(Color("blue"), 3)
+            Line(ctx).of_start_end(in_pos1, in_pos1 + V(-30, 0)).stroke(Color("blue"), 3)
             Line(ctx).of_start_end(out_pos, out_pos + V(50, 0)).stroke(Color("green"), 3)
 
-            Text(ctx).of("C", in_pos).size(20).align(RIGHT, BOTTOM).offset(-10, -5).fill(Color(0))
+            Text(ctx).of("C", in_pos1).size(20).align(RIGHT, BOTTOM).offset(-10, -5).fill(Color(0))
             Text(ctx).of("D", out_pos).size(20).align(LEFT, BOTTOM).offset(10, -5).fill(Color(0))
             Text(ctx).of("Y", gate.label_pos()).size(20).align(CENTER, MIDDLE).fill(Color(0))
 
         def creator(file):
-            make_image(file, draw, 800, 400)
+            make_image(file, draw, 400, 300)
 
         self.assertTrue(run_image_test('test_buffer_gate.png', creator))
+
+    def test_and_gate(self):
+        def draw(ctx, width, height, frame_no, frame_count):
+            setup(ctx, width, height, background=Color(1))
+
+            gate = And((100, 100), 50).fillstyle(Color("yellow")).strokestyle(Color("blue"), 3)
+            gate.draw(ctx)
+            in_pos1 = V(gate.get_connector(0, 0))
+            in_pos2 = V(gate.get_connector(0, 1))
+            out_pos = V(gate.get_connector(1, 0))
+            Line(ctx).of_start_end(in_pos1, in_pos1 + V(-30, 0)).stroke(Color("blue"), 3)
+            Line(ctx).of_start_end(in_pos2, in_pos2 + V(-30, 0)).stroke(Color("blue"), 3)
+            Line(ctx).of_start_end(out_pos, out_pos + V(50, 0)).stroke(Color("green"), 3)
+
+            Text(ctx).of("A", in_pos1).size(20).align(RIGHT, BOTTOM).offset(-10, -5).fill(Color(0))
+            Text(ctx).of("B", in_pos2).size(20).align(RIGHT, TOP).offset(-10, 5).fill(Color(0))
+            Text(ctx).of("C", out_pos).size(20).align(LEFT, BOTTOM).offset(10, -5).fill(Color(0))
+            Text(ctx).of("X", gate.label_pos()).size(20).align(CENTER, MIDDLE).fill(Color(0))
+
+            gate = And((100, 200), 50, invert=True).fillstyle(Color("yellow")).strokestyle(Color("blue"), 3)
+            gate.draw(ctx)
+            in_pos1 = V(gate.get_connector(0, 0))
+            in_pos2 = V(gate.get_connector(0, 1))
+            out_pos = V(gate.get_connector(1, 0))
+            Line(ctx).of_start_end(in_pos1, in_pos1 + V(-30, 0)).stroke(Color("blue"), 3)
+            Line(ctx).of_start_end(in_pos2, in_pos2 + V(-30, 0)).stroke(Color("blue"), 3)
+            Line(ctx).of_start_end(out_pos, out_pos + V(50, 0)).stroke(Color("green"), 3)
+
+            Text(ctx).of("D", in_pos1).size(20).align(RIGHT, BOTTOM).offset(-10, -5).fill(Color(0))
+            Text(ctx).of("E", in_pos2).size(20).align(RIGHT, TOP).offset(-10, 5).fill(Color(0))
+            Text(ctx).of("F", out_pos).size(20).align(LEFT, BOTTOM).offset(10, -5).fill(Color(0))
+            Text(ctx).of("Y", gate.label_pos()).size(20).align(CENTER, MIDDLE).fill(Color(0))
+
+        def creator(file):
+            make_image(file, draw, 400, 300)
+
+        self.assertTrue(run_image_test('test_and_gate.png', creator))
