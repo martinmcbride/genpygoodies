17767c2f713440a9beacc8bb1d587c9cc239941e
martinmcbride <mcbride.martin@gmail.com>
2025-04-09 11:59:00 +0100

    Fix image sizes in formula tests

--- imagetests/test_formulas_module.py
+++ imagetests/test_formulas_module.py
@@ -17,7 +17,7 @@ class TestFormulas(unittest.TestCase):
         def creator(file):
             width, height = make_formulas_png(file, [r"x^2"], Color("red"))
             self.assertEquals(width, 174)
-            self.assertEquals(height, 172)
+            self.assertEquals(height, 174)
 
         self.assertTrue(run_image_test('test_single_formula.png', creator))
 
@@ -26,7 +26,7 @@ class TestFormulas(unittest.TestCase):
         def creator(file):
             width, height = make_formulas_png(file, [r"c^2 = a^2 + b^2", r"\frac{a+b}{c+d} + 3.141592654", r"\frac{a+b}{c+d} + 3.141592654", r"c^2 = a^2 + b^2"], Color("black"), dpi=300, gap=30, background=Color("cadetblue"))
             self.assertEquals(width, 424)
-            self.assertEquals(height, 402)
+            self.assertEquals(height, 404)
 
         self.assertTrue(run_image_test('test_multiple_formulas.png', creator))
 
